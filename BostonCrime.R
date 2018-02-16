3db567837793c5aa6946ef64e65b02ede92cfdc7
Nikilesh Kumar <kumar.ni@husky.neu.edu>
2018-02-16 16:27:01 -0500

    Added District wise crime split and modified map display

--- BostonCrime.R
+++ BostonCrime.R
@@ -1,7 +1,18 @@
 library(ggmap)
-crimedb<-read.csv("crime.csv")
 library(plyr)
+library(lubridate)
+crimedb<-read.csv("crime.csv")
+#remove all incomplete cases
 crimeclean<-crimedb[complete.cases(crimedb),]
+#Add original district names to all district numbers
+crimeclean$origdistrictname <- mapvalues(crimeclean$DISTRICT, from = c('A1','A15','A7','B2','B3','C6','C11','D4','D14','E5','E13','E18'), to=c('DT & Charleston','DT & Charleston','East Boston','Roxbury','Mattapan','South Boston','Dorchester','South End','Brighton','West Roxbury','Jamaica Plain','Hyde Park'))
+#Split dataset for data from 2017 - 2018 Feb
+lastyear <- crimeclean %>% select(INCIDENT_NUMBER,OFFENSE_CODE,OFFENSE_CODE_GROUP,OFFENSE_DESCRIPTION,DISTRICT,REPORTING_AREA,SHOOTING,OCCURRED_ON_DATE,YEAR,MONTH,DAY_OF_WEEK,HOUR,UCR_PART,STREET,Lat,Long,Location,origdistrictname) %>%
+  filter(between(as.Date(OCCURRED_ON_DATE), as.Date("2017-02-01"), as.Date("2018-02-01")))
+#Split dataset for data from 2016 - 2017 Feb
+yearbeforelast <- crimeclean %>% select(INCIDENT_NUMBER,OFFENSE_CODE,OFFENSE_CODE_GROUP,OFFENSE_DESCRIPTION,DISTRICT,REPORTING_AREA,SHOOTING,OCCURRED_ON_DATE,YEAR,MONTH,DAY_OF_WEEK,HOUR,UCR_PART,STREET,Lat,Long,Location,origdistrictname) %>%
+  filter(between(as.Date(OCCURRED_ON_DATE), as.Date("2016-02-01"), as.Date("2017-02-01")))
+#Get Map of Boston
 bostonmap <- get_map(location = 'boston', zoom = 12, maptype = 'roadmap')
 #Roadmap is only available from Google Maps
 interestedregion <- ggmap(bostonmap)+
@@ -9,15 +20,42 @@ interestedregion <- ggmap(bostonmap)+
   #scale_y_continuous(limits = c(42.26, 42.42), expand = c(0, 0)) +
   #Include the above two lines if you want to zoom in to just the Boston region
   #Create the density plots for crime regions. Bin size is responsible for resolution
-  stat_density2d(data = crimeclean, aes(x=crimeclean$Long,y=crimeclean$Lat,alpha=..level..,fill=..level..), bins=18,geom='polygon')+
+  stat_density2d(data = lastyear, aes(x=lastyear$Long,y=lastyear$Lat,alpha=..level..,fill=..level..), bins=12,geom='polygon')+
   #Crime Density scale in range of blue to orange to denote intensity
-  scale_fill_gradient('Crime\nDensity', low = 'blue', high = 'orange') +
+  scale_fill_gradient('Crime\nDensity', low = 'darkmagenta', high = 'gold') +
   #Populate map with the density color scheme
   scale_alpha(range = c(.2, .3), guide = FALSE)+
   guides(fill = guide_colorbar(barwidth = 1.5, barheight = 10)) +
   #Remove the axes titles long and lat
   theme(axis.title.y = element_blank(), axis.title.x = element_blank()) +
   #Add title to map
-  ggtitle("Boston Crime February 2018")
+  ggtitle("Boston Crime February 2017 - February 2018")
 interestedregion
-
+#Create a table for district wise count of crimes
+districtwise <- as.data.frame(table(lastyear$origdistrictname))
+#Rename the columns
+names(districtwise)<-c('District','Frequency')
+library(ggplot2)
+#Plot the Barplot for the table created. Exclude the first column as it contains cases with no district
+yearof2017 <- ggplot(districtwise[-1,], aes(x=District, y=Frequency,fill=District)) +
+  geom_bar(stat = "identity") +
+  theme_minimal() +
+  ggtitle("Crime By District - Boston - (Feb 2017-2018)")
+yearof2017
+#Do the same for the 2016 year using the yearbeforelast dataframe
+prevyr <- as.data.frame(table(yearbeforelast$origdistrictname))
+names(prevyr)<- c('District','Frequency')
+yearof2016 <- ggplot(prevyr[-1,], aes(x=District, y=Frequency,fill=District)) +
+  geom_bar(stat = "identity") +
+  theme_minimal() +
+  ggtitle("Crime By District - Boston - (Feb 2016-2017)")
+yearof2016
+#Calculate Percentage change between the two datasets
+crimechange <- data.frame(districtwise$District,(districtwise$Frequency-prevyr$Frequency)*100/prevyr$Frequency)
+names(crimechange) <- c('District','PctChange')
+#Plot percentage change
+pctchange <-  ggplot(crimechange[-1,], aes(x=District, y=PctChange,fill=District)) +
+  geom_bar(stat = "identity") +
+  theme_minimal() +
+  ggtitle("Pct Change in Crime - Boston - (2016-17 vs 2017-18)")
+pctchange
